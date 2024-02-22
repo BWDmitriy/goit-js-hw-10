2279b8c76c986dcd1347f7787f2b070183ea44f9
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-02-22 21:58:52 +0200

    Task 2 breakthrough

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -11,12 +11,33 @@ function createNotification(e) {
   e.preventDefault();
   let stateInput = document.querySelector('input[name="state"]:checked');
   if (delayInput.value && stateInput.value) {
-    console.log('delayInput and stateInput are true!');
-    return alert('ok');
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (stateInput.value == 'fulfilled') {
+          resolve('Success! Value passed to resolve function!');
+          console.log(promise);
+          iziToast.success({
+            title: 'Success!',
+            message: `Fulfilled promise in ${delayInput.value}ms`,
+            position: 'topRight',
+          });
+        }
+        if (stateInput.value == 'rejected') {
+          reject('Error! Error passed to reject function!');
+          console.log(promise);
+          iziToast.error({
+            title: 'Error',
+            message: 'Error passed to reject function!',
+            position: 'topRight',
+          });
+        }
+      }, delayInput.value);
+    });
+    return promise;
   }
   return iziToast.warning({
     title: 'Warning',
-    message: 'You forgot important data',
+    message: 'You forgot important data!',
     position: 'topRight',
   });
 }
